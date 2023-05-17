7476664e64e6267537b21cf0451721e6170eba89
jtcannobbio <tomas.cannobbio@gmail.com>
2023-05-17 16:26:09 -0400

    last commit

--- index.js
+++ index.js
@@ -14,29 +14,34 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({message: "Algo salio mal: internal server error"});
+});
 
 // get method
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   try {
     res.sendFile(path.join(__dirname, "public", "index.html"));
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    next(err);
   }
 });
 
-app.get("/canciones", async (req, res) => {
+app.get("/canciones", async (req, res, next) => {
   try {
     const songs = await readFile(songsFilePath, "utf-8");
 
     res.json(JSON.parse(songs));
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    next(err);
   }
 });
 
 // post method
 
-app.post("/canciones", async (req, res) => {
+app.post("/canciones", async (req, res, next) => {
   try {
     const newSong = req.body;
     if (
@@ -54,15 +59,15 @@ app.post("/canciones", async (req, res) => {
     await writeFile(songsFilePath, JSON.stringify(songsArray));
     res.status(201).json(songsArray);
 
-  } catch (error) {
+  } catch (err) {
     
-    console.log(error);
+    next(err);
   }
 });
 
 // delete method
 
-app.delete("/canciones/:id", async (req, res) => {
+app.delete("/canciones/:id", async (req, res, next) => {
   try {
     const {id} = req.params;
     const songs = await readFile(songsFilePath, "utf-8");
@@ -70,14 +75,14 @@ app.delete("/canciones/:id", async (req, res) => {
     const newSongsArray = songsArray.filter((song) => song.id !== parseInt(id));
     await writeFile(songsFilePath, JSON.stringify(newSongsArray));
     res.json(newSongsArray);
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    next(err);
   }
 });
 
 // put method
 
-app.put("/canciones/:id", async (req, res) => {
+app.put("/canciones/:id", async (req, res, next) => {
   try {
     const {id} = req.params;
     const {titulo, artista, tono} = req.body;
@@ -96,7 +101,8 @@ app.put("/canciones/:id", async (req, res) => {
     });
     await writeFile(songsFilePath, JSON.stringify(newSongsArray));
     res.status(201).json(newSongsArray);
-  } catch (error) {
-    console.log(error);
+
+  } catch (err) {
+    next(err);
   }
 });
